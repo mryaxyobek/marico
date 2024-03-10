a06c1f7b5b55053b3ae02b59b04a037d783e1b9f
mryaxyobek <pubgn9642@gmail.com>
2024-03-10 21:50:09 +0500

    add brand colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.{html,js}"],
+  content: ["./src/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: {
+          dark: {
+            950: "#000000",
+            900: "#0E0E0E",
+          },
+
+          blue: {
+            500: "#326CF9",
+          },
+
+          gray: {
+            600: "#707070",
+            500: "#919191",
+          },
+          green: "#0FC65C",
+        },
+      },
+    },
   },
   plugins: [],
 };
-
