6b4c770346e7530efd3c5de51c7279a597b596a6
mryaxyobek <pubgn9642@gmail.com>
2024-03-12 08:37:46 +0500

    header is optimized for all devices

---
+++ src/js/main.js
@@ -0,0 +1,5 @@
+const elHamburgerBtn = document.querySelector(".js-hamburger-btn");
+
+elHamburgerBtn.addEventListener("click", () => {
+  elHamburgerBtn.classList.toggle("active-hamburger-btn");
+});
